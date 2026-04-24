27dc4a996d266065a0ddee4a4a75189d4f3671d9
xXMRK888YTXx <xxmrk888ytxx.rlc@gmail.com>
2026-04-24 21:42:27 +0300

    [Chore] Increment versionCode to 4
    - Update `versionCode` from 3 to 4 in `app/build.gradle.kts`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.xxmrk888ytxx.apexowner"
         minSdk = catalogMinSdk
         targetSdk = catalogCompileSdk
-        versionCode = 3
+        versionCode = 4
         versionName = "WIP"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
